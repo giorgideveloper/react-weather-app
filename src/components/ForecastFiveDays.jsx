277bf55fe03798566f062693e6e8499cc0836359
giorgideveloper <gorgiladze888@gmail.com>
2022-11-04 03:31:12 +0400

    add five day style

--- src/components/ForecastFiveDays.jsx
+++ src/components/ForecastFiveDays.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import fiveDayIcon from '../assets/img/fiveDayIcon.svg';
 
 function ForecastFiveDays() {
-	return <div></div>;
+	return (
+		<>
+			{' '}
+			<Row className='pt-3 pb-5 justify-content-center text-center text-light'>
+				<Col md={2}>
+					<div
+						style={{
+							width: '100%',
+							height: '230px',
+							background: 'rgba(255, 255, 255, 0.2)',
+						}}
+					>
+						{' '}
+						<p
+							style={{
+								paddingTop: '20px',
+								fontSize: '24px',
+								fontWeight: 'bold',
+							}}
+						>
+							24°C
+						</p>
+						<img style={{ paddingTop: '5px' }} src={fiveDayIcon} alt='' />
+						<p
+							style={{
+								paddingTop: '10px',
+								fontSize: '24px',
+								fontWeight: 'bold',
+							}}
+						>
+							Fri
+						</p>
+					</div>
+				</Col>{' '}
+			</Row>
+		</>
+	);
 }
 
 export default ForecastFiveDays;
--- src/components/ForecastToday.jsx
+++ src/components/ForecastToday.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import WeatherIcon from '../assets/img/weather-icon.png';
+import icon1 from '../assets/img/icon1.svg';
+import icon2 from '../assets/img/icon2.svg';
+import icon3 from '../assets/img/icon3.svg';
 
 function ForecastToday() {
 	return (
 		<>
-			<Row className='justify-content-center' style={{ marginTop: '150px' }}>
+			<Row className='justify-content-center' style={{ marginTop: '60px' }}>
 				<Col md={4} offset-md={2}>
 					<div
 						className='justify-content-center d-flex text-light'
@@ -18,13 +21,39 @@ function ForecastToday() {
 				<Col md={3}>
 					<img src={WeatherIcon} width='100%' alt='weather icon' />
 				</Col>
-				<Col md={12} className='text-light '>
+			</Row>
+			<Row>
+				<Col md={3} offset-md={1}></Col>
+				<Col md={8} className='text-light '>
 					{' '}
 					<div className=''>
 						<p style={{ fontSize: '30px' }}>17th Jun ‘21</p>
 						<p style={{ fontSize: '20px' }}>Thrusday 2:45 am</p>
 					</div>
-					<p>Wind 10 km/h Hum 54 % Rain 0.2 %</p>
+				</Col>
+			</Row>
+			<Row>
+				<Col md={12} className='justify-content-center d-flex'>
+					<Col md={6}>
+						{' '}
+						<ul
+							style={{ listStyle: 'none' }}
+							className='d-flex justify-content-between text-light'
+						>
+							<li className=''>
+								{' '}
+								<img src={icon3} alt='Wind' /> Wind 10 km/h
+							</li>
+							<li className='border-start ps-5 border-end pe-5'>
+								{' '}
+								<img src={icon2} alt='Hum' /> Hum 54k %
+							</li>
+							<li>
+								{' '}
+								<img src={icon1} alt='Rain' /> Rain %
+							</li>
+						</ul>
+					</Col>
 				</Col>
 			</Row>
 		</>
